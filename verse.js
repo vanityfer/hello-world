b0a1bb4a23020b5ec36d8476650ab5f2ccefa9ad
vanityfer <75648982+vanityfer@users.noreply.github.com>
2021-02-20 22:51:19 -0300

    Update verse.js

--- verse.js
+++ verse.js
@@ -7,13 +7,13 @@ let NAMES = [
 
 let VERSES = {
   "soñé": [
-    "película cuarto de libra con queso cómo se llama",
-    "tiempos violentos película completa",
-    "tiempos violentos película completa español latino",
-    "tiempos violentos película completa español latino gratis",
+    "que ksjdalskdjalskdjasd",
+    "que kdajsdlkajsd",
+    "que ldkjalsdjlaskjdlakjsdlaksjdlkasdino",
+    "que no",
     "",
-    "búsquedas raras en google",
-    "búsquedas graciosas en google",
+   "búsquedas raras en google",
+    "búsqu edas graciosas en google",
     "búsquedas más frecuentes en google 2020",
     "cuál es la mejor alternativa a google",
     "",
@@ -27,48 +27,6 @@ let VERSES = {
     "quién escribió voy a tener suerte",
     "análisis voy a tener suerte"
   ],
-  "soñé": [
-    "no me viene",
-    "no me viene hace 2 meses",
-    "no me viene hace 2 meses y tomo pastillas",
-    "no me viene hace 2 meses y tomo pastillas test negativo",
-    "",
-    "pies hinchados",
-    "pies hinchados embarazo",
-    "pies hinchados embarazo es normal",
-    "cómo deshinchar pies hinchados embarazo",
-    "",
-    "recién nacido peso",
-    "recién nacido bajo peso",
-    "recién nacido bajo peso extremo",
-    "recién nacido bajo peso extremo riesgos",
-    "",
-    "duelo perinatal",
-    "tasa de mortalidad perinatal",
-    "tasa de mortalidad perinatal argentina",
-    "psicóloga duelo perinatal"
-  ],
-  "soñé": [
-    "mujer gritándole a gato",
-    "mujer gritándole a gato meme",
-    "mujer gritándole a gato meme origen",
-    "crear meme mujer gritándole a gato",
-    "",
-    "pepa es lo mismo que lsd",
-    "pepa droga es adictiva",
-    "pepa droga duración",
-    "cuánto dura el efecto de 1/4 de pepa",
-    "",
-    "por qué soy yo",
-    "por qué yo soy yo",
-    "por qué yo soy yo y no soy otra persona",
-    "por qué estoy acá y no estoy en otro momento",
-    "",
-    "cuando el cuerpo muere la mente sigue",
-    "puede seguir la conciencia después de la muerte",
-    "qué cambia en el instante en el que una persona muere",
-    "por qué una persona viva se convierte en una persona muerta"
-  ],
- 
+   
 };
 
